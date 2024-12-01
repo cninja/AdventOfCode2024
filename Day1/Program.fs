7299882f451f9c38f5b330bb63774b46620a6516
cninja <cninja@gmail.com>
2024-12-01 14:45:14 -0600

    Add project files.

---
+++ Day1/Program.fs
@@ -0,0 +1,14 @@
+﻿let array1, array2 = 
+    System.IO.File.ReadAllLines "input"
+    |> Seq.map (fun line -> line.Split(' ') |> Seq.filter (fun i -> not(System.String.IsNullOrWhiteSpace(i))) |> Seq.map int |> Seq.toArray)
+    |> Seq.map (fun line -> (line.[0], line.[1]))
+    |> Seq.toArray
+    |> Array.unzip
+
+let ans = 
+    Array.zip (Array.sort array1) (Array.sort array2)
+    |> Seq.map (fun (a, b) -> System.Math.Abs(a - b))
+    |> Seq.sum
+
+printfn $"The anser is: {ans}"
+
---
+++ Part2/Program.fs
@@ -0,0 +1,22 @@
+﻿let example = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+
+let array1, array2 = 
+    System.IO.File.ReadAllLines "input"
+    //example.Split '\n'
+    |> Seq.map (fun line -> line.Split(' ') |> Seq.filter (fun i -> not(System.String.IsNullOrWhiteSpace(i))) |> Seq.map int |> Seq.toArray)
+    |> Seq.map (fun line -> (line.[0], line.[1]))
+    |> Seq.toArray
+    |> Array.unzip
+
+let ans = 
+    array1
+    |> Array.map (fun i -> array2 |> Array.map (fun j -> if i = j then i else 0) |> Seq.sum)
+    |> Seq.sum
+
+printfn $"The anser is: {ans}"
+
