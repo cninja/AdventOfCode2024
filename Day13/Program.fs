7ef58b2379c547ad2612c495d9dd6b9aea82309c
psiewert <psiewert@triumphpay.com>
2024-12-14 08:16:37 -0600

    Days 13 and 14

---
+++ Day13/Program.fs
@@ -0,0 +1,73 @@
+﻿
+open System
+open System.Text.RegularExpressions
+
+let input = "Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"
+
+let inputFile = System.IO.File.ReadAllText "input"
+
+type Machine =
+    struct
+        val buttonA: (decimal*decimal)
+        val buttonB: (decimal*decimal)
+        val prize: (decimal*decimal)
+        new (ax, ay, bx, by, px, py) =           // Constructor definition
+           {buttonA = (decimal ax, decimal ay)
+            buttonB = (decimal bx, decimal by)
+            prize = (decimal px, decimal py);} 
+    end
+type Play = (decimal*decimal)
+
+let playMachine (m:Machine): Play option =
+    let (ax, ay) = m.buttonA
+    let (bx, by) = m.buttonB
+    let (px_part, py_part) = m.prize
+    //Part2
+    let px = px_part + 10000000000000.0m
+    let py = py_part + 10000000000000.0m
+    
+    let s1 = py / ay
+    let i = ((py / ay) - (by * px) / (ay * bx)) / ((decimal 1) - (by * ax) / (ay * bx))
+    let j = (px - i*ax)/bx
+    
+    let ri = Math.Round i
+    let rj = Math.Round j
+    if Math.Abs(i - ri) < 0.00001m && Math.Abs(j - rj) < 0.00001m then
+        Some (ri, rj)
+    else
+        None
+    
+    
+let costForPlay (p:Play) =
+    let(a,b) = p
+    a * 3m + b
+    
+    
+let machines =
+    Regex.Matches(inputFile, @"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)")
+    |> Seq.map (fun m -> Machine(m.Groups.[1].Value, m.Groups.[2].Value, m.Groups.[3].Value, m.Groups.[4].Value, m.Groups.[5].Value, m.Groups.[6].Value))
+    |> Seq.choose playMachine
+    |> Seq.toArray
+
+let ans =
+    machines
+    // |> Seq.length
+    |> Seq.map costForPlay
+    |> Seq.sum
+    
+    
+printfn $"Ans: {ans}"
---
+++ Day14/Program.fs
@@ -0,0 +1,95 @@
+﻿
+open System
+open System.Collections
+open System.Text.RegularExpressions
+
+let input = "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"
+
+let inputFile = System.IO.File.ReadAllText "input"
+
+type Robot = ((int * int) * (int * int))
+
+// let roomX = 11
+// let roomY = 7
+let roomX = 101
+let roomY = 103
+let robots =
+    Regex.Matches(inputFile, @"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)")
+    |> Seq.map (fun m -> ((int m.Groups.[1].Value, int m.Groups.[2].Value),(int m.Groups.[3].Value, int m.Groups.[4].Value)))
+    |> Seq.toArray
+
+let modu x n = ((x % n) + n) % n
+
+let addSeconds (r:Robot) (s: int):(int*int) =
+    let ((x,y),(dx,dy)) = r
+    let newX = x + (dx * s)
+    let newY = y + (dy * s)
+    (modu newX roomX, modu newY roomY)
+    
+let exampleRobot = ((2,4),(2,-3))
+let setps =
+    {0..5}
+    |> Seq.map (addSeconds exampleRobot)
+    |> Seq.toArray
+    
+let finalPosition =
+    robots
+    |> Seq.map (fun r -> addSeconds r 100)
+    |> Seq.toArray
+
+
+let quadLimitX = roomX / 2 
+let quadLimitY = roomY / 2
+
+let quadCount fn1 fn2 =
+    finalPosition
+    |> Seq.where (fun p ->
+        let (x, y) = p
+        fn1 x quadLimitX && fn2 y quadLimitY)
+    |> Seq.length
+let q1 = quadCount (<) (<)
+let q2 = quadCount (<) (>)
+let q3 = quadCount (>) (<)
+let q4 = quadCount (>) (>)
+let ans = q1 * q2 * q3 * q4
+printfn $"Ans: {ans}"
+
+let printPositionsAt s =
+    let positionLookup = 
+        robots
+        |> Seq.map (fun r ->
+            let (x, y) = addSeconds r s
+            x * 1000 + y)
+        |> Set.ofSeq
+        
+    
+    let room =
+        {0..roomY-1}
+        |> Seq.map (fun y ->
+            {0..roomX-1}
+            |> Seq.map (fun x ->
+                if positionLookup.Contains (x * 1000 + y) then '1' else '.')
+            |> String.Concat)
+        |> (String.concat "\n")
+    
+    let streaks = Regex.Matches(room, @"11111").Count
+    if streaks > 5 then
+        printfn $"Time: {s}\n{room}\n\n"
+    else if s % 1000 = 0 then
+        printfn $"Tick: {s}"
+    
+{0..100000}
+    |> Seq.map printPositionsAt
+    |> Seq.toArray
+    |> ignore
